b7749c4ce2be7acd29c4f72842eaa5d2ec8cd579
sarika891 <46074252+sarika891@users.noreply.github.com>
2021-06-29 00:29:14 +0200

    Update MediclaimApplication.java

--- src/main/java/com/strikers/mediclaim/MediclaimApplication.java
+++ src/main/java/com/strikers/mediclaim/MediclaimApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnablePrometheusMetrics
 public class MediclaimApplication {
 
 	public static void main(String[] args) {
